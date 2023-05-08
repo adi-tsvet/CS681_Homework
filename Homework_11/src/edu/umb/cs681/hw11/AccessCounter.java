2eb3aad6486cb5b6b82fed9c3a25a9d8eb801abc
Adnan Ali <48458068+adi-tsvet@users.noreply.github.com>
2023-05-08 15:23:29 -0400

    AccessCounter-ThreadSafe

---
+++ Homework_11/src/edu/umb/cs681/hw11/AccessCounter.java
@@ -0,0 +1,59 @@
+package edu.umb.cs681.hw11;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AccessCounter {
+    private static AccessCounter instance = null;
+    private Map<Path, Integer> accessCount;
+    private static Lock lock = new ReentrantLock();
+    private Lock nonStaticLock;
+    private AccessCounter() {
+        accessCount = new HashMap<>();
+        nonStaticLock = new ReentrantLock();
+    }
+
+    public static AccessCounter getInstance() {
+        if (instance == null) {
+            lock.lock();
+            try {
+                if (instance == null) {
+                    instance = new AccessCounter();
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+        return instance;
+    }
+
+    public void increment(Path path) {
+        nonStaticLock.lock();
+        try {
+            if (accessCount.containsKey(path)) {
+                accessCount.put(path, accessCount.get(path) + 1);
+            } else {
+                accessCount.put(path, 1);
+            }
+        } finally {
+            nonStaticLock.unlock();
+        }
+    }
+
+    public int getCount(Path path) {
+        nonStaticLock.lock();
+        try {
+            if (accessCount.containsKey(path)) {
+                return accessCount.get(path);
+            } else {
+                return 0;
+            }
+        } finally {
+            nonStaticLock.unlock();
+        }
+    }
+}
+
