b6eaa0c89eb4f5c92fb49ee34a26402063eb092f
Adnan Ali <48458068+adi-tsvet@users.noreply.github.com>
2023-05-09 16:43:57 -0400

    PrimeFactorizer-ThreadSafe

--- Homework_06-2/src/edu/umb/cs681/hw602/RunnableCancellablePrimeFactorizer.java
+++ Homework_06-2/src/edu/umb/cs681/hw602/RunnableCancellablePrimeFactorizer.java
@@ -1,11 +1,14 @@
 package edu.umb.cs681.hw602;
 
+
+import edu.umb.cs681.hw08.RunnablePrimeFactorizer;
+
 import java.util.concurrent.locks.ReentrantLock;
 
 public class RunnableCancellablePrimeFactorizer extends RunnablePrimeFactorizer {
 
-    private boolean done = false;
-    private ReentrantLock lock = new ReentrantLock();
+    public boolean done = false;
+    public ReentrantLock lock = new ReentrantLock();
 
     public RunnableCancellablePrimeFactorizer(long dividend, long from, long to) {
         super(dividend, from, to);
@@ -21,12 +24,15 @@ public class RunnableCancellablePrimeFactorizer extends RunnablePrimeFactorizer 
     }
 
     public void generatePrimeFactors() {
-
-
-            long divisor = from;
-            while (dividend != 1 && divisor <= to && !done) {
+        long divisor = from;
+            while (dividend != 1 && divisor <= to) {
                 try {
                     lock.lock();
+                    if(done){
+                        System.out.println("Stopped generating prime Factors.");
+                        this.factors.clear();
+                        break;
+                    }
                     if (divisor > 2 && isEven(divisor)) {
                         divisor++;
                         continue;
