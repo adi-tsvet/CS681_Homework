c7f69f1eef05e5105d590898c28a8ff76d6861b1
Adnan Ali <48458068+adi-tsvet@users.noreply.github.com>
2023-05-13 21:34:04 -0400

    Aircraft-ThreadSafe-Immutable

--- Homework_09/src/edu/umb/cs681/hw09/AircraftMainTest.java
+++ Homework_09/src/edu/umb/cs681/hw09/AircraftMainTest.java
@@ -3,10 +3,13 @@ package edu.umb.cs681.hw09;
 public class AircraftMainTest {
     public static void main(String[] args) throws InterruptedException {
         Aircraft aircraft = new Aircraft(new Position(0, 0, 0));
-        Runnable aircraftRunnable = new AircraftRunnable(aircraft);
 
         Thread[] threads = new Thread[5];
         for (int i = 0; i < threads.length; i++) {
+            //As per homework correction,
+            // Now, each Thread is associated with a unique AircraftRunnable instance,
+            // ensuring that they don't share the same Runnable object.
+            Runnable aircraftRunnable = new AircraftRunnable(aircraft);
             threads[i] = new Thread(aircraftRunnable);
             threads[i].setName("Thread " + i);
             threads[i].start();
