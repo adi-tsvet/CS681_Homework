e62242995db54b9c12558b5ad95d48ef010b3313
Adnan Ali <48458068+adi-tsvet@users.noreply.github.com>
2023-05-17 00:23:46 -0400

    StockQuoteObservable-ThreadSafe-OpenCall

--- Homework_15/src/edu/umb/cs681/hw15/TestStockQuoteObservable.java
+++ Homework_15/src/edu/umb/cs681/hw15/TestStockQuoteObservable.java
@@ -38,15 +38,5 @@ public class TestStockQuoteObservable {
         for (Thread thread : threads) {
             thread.interrupt();
         }
-
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-
     }
 }
